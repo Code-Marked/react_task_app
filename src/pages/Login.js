f8c52677f34f8f5c9309c3a14d6fec0e78b4ad5a
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-12-15 16:16:14 -0600

    Sends and recieves login json details to api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react'
 
-function Login() {
-    return (
-        <h1>Login Page</h1>
-    );
-}
+const Login=()=>{
+    const [message, setMessage] = useState("");
+	const [username,setUsername]=useState(""); 
+	const [password,setPassword]=useState(""); 
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("http://localhost:5000/login", {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    username:username,
+                    password:password,
+                }),
+            });
+            let resJson = await res.json();
+                if (res.status === 200) {
+                setUsername("");
+                setPassword("");
+                setMessage("Log In Successful");
+                } else {
+                    setMessage("Some error occured");
+                }
+        } catch (err) {
+          console.log(err);
+        }
+    };
+
+	return(
+	<div>
+		<form onSubmit={handleSubmit}> 
+			<div> 
+				<label htmlFor="username">Username</label>
+				<input type="text" name="username" id="username" value={username} onChange={(e)=>setUsername(e.target.value)}/> 
+			</div> 
+			<div> 
+				<label htmlFor="password">Password</label>
+			<input type="text" name="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/> 
+			</div>
+			<button type="submit">Login</button>
+            <div className="message">{message ? <p>{message}</p> : null}</div>
+            
+		</form>
+	</div>
+)} 
 
 export default Login;
